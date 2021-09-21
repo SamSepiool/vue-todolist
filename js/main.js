ad71627c236c3c17fb0d5268af018d39ee48026c
SamSepiool <simone.severini@outlook.it>
2021-09-21 10:13:53 +0200

    add check button

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ const app = new Vue({
     el: root,
 
     data: {
-        title: 'ToDo-List',
         toDos:[
             {
                 task:'Fare i compiti',
@@ -17,6 +16,30 @@ const app = new Vue({
                 task:'Fare il bucato',
                 done: false
             },
-        ]
+        ],
+    
+        newTodo:'',
+    },
+    methods:{
+
+        addTodo: function(){
+            if(this.newTodo != ''){
+                let transferObj = {task: this.newTodo, done: false};
+                this.toDos.push(transferObj);
+                this.newTodo = '';
+            }
+        },
+
+        deleteTodo: function(index){
+            this.toDos.splice(index, 1);
+        },
+
+        checkTodo: function (index){
+            
+            (this.toDos[index].done == false) ? this.toDos[index].done = true : alert('task già completata')
+            console.log(this.toDos[index].done)
+                   
+        }
+
     }
 })
